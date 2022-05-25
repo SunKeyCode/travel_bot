e16bf439650703eb6997971e3c81d52f7c349b8a
Aleksandr Kurusin <stratdn@mail.ru>
2022-05-25 17:58:26 +0400

    1st commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import telebot
+import requests
 
+
+url = "https://hotels4.p.rapidapi.com/locations/v2/search"
+
+querystring = {"query": "new york", "locale": "en_US", "currency": "USD"}
+
+headers = {
+    "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
+    "X-RapidAPI-Key": "bae1651a05msh66bdf614f9bc0e9p1a06dfjsn581c7e145db3"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
